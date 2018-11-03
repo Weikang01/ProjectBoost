f837a0afd8255673a4dcf2bb813997bfa6e89c2d
Weikang01 <Weikang01@users.noreply.github.com>
2018-11-03 18:37:10 +0800

    debug unfreeze rotation

--- Assets/Script/Rocket.cs
+++ Assets/Script/Rocket.cs
@@ -67,17 +67,22 @@ public class Rocket : MonoBehaviour {
 
     void RespondToRotateInput()
     {
-        rb.freezeRotation = true;
         float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * rotationThisFrame);
+            RotateManually(rotationThisFrame);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward * rotationThisFrame);
+            RotateManually(-rotationThisFrame);
         }
+    }
 
+    private void RotateManually(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame);
         rb.freezeRotation = false;
     }
 
